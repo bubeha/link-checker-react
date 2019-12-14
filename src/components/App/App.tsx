ae3d872ac67abf3a477993675b22f0e74c09ebff
Aleksandr Bubeshko <a.bubeshko@gmail.com>
2019-12-14 22:20:34 +0300

    add login and registration pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import WelcomePage from '../../pages/WelcomePage';
 import NavBar from "../NavBar";
+import LoginPage from "../../pages/auth/LoginPage";
+import RegistrationPage from "../../pages/auth/RegistrationPage";
 
 const App: React.FC = () => {
   return (
@@ -10,6 +12,8 @@ const App: React.FC = () => {
       <div className="container">
         <Switch>
           <Route component={WelcomePage} path="/" exact/>
+          <Route component={LoginPage} path="/login"/>
+          <Route component={RegistrationPage} path="/registration" />
         </Switch>
       </div>
     </BrowserRouter>
---
+++ src/pages/auth/RegistrationPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const RegistrationPage: React.FC = () => (
+  <div>
+    <h1>Registration</h1>
+  </div>
+);
+
+export default RegistrationPage;
