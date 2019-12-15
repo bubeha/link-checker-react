4c1f55a7c9eebde37184487822626cfe4bfb32a1
Aleksandr Bubeshko <a.bubeshko@gmail.com>
2019-12-15 20:01:35 +0300

    move provider and Browser router from app component to index.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,33 +1,16 @@
 import React from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import WelcomePage from '../../pages/WelcomePage';
-import NavBar from "../NavBar";
 import LoginPage from "../../pages/auth/LoginPage";
 import RegistrationPage from "../../pages/auth/RegistrationPage";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from '../../stores/reducers';
-import { devToolsEnhancer } from 'redux-devtools-extension';
-
-const store = createStore(
-  rootReducer,
-  devToolsEnhancer({}),
-);
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Provider store={store}>
-        <NavBar/>
-        <div className="container">
-          <Switch>
-            <Route component={WelcomePage} path="/" exact/>
-            <Route component={LoginPage} path="/login"/>
-            <Route component={RegistrationPage} path="/registration"/>
-          </Switch>
-        </div>
-      </Provider>
-    </BrowserRouter>
+    <React.Fragment>
+      <Route component={WelcomePage} path="/" exact/>
+      <Route component={LoginPage} path="/login"/>
+      <Route component={RegistrationPage} path="/registration"/>
+    </React.Fragment>
   );
 };
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom';
 import './styles/index.scss';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
+import {Provider} from "react-redux";
+import NavBar from "./components/NavBar";
+import {BrowserRouter, Switch} from "react-router-dom";
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+import {createStore} from 'redux';
+import rootReducer from 'stores/reducers';
+import {devToolsEnhancer} from 'redux-devtools-extension';
+
+const store = createStore(
+  rootReducer,
+  devToolsEnhancer({}),
+);
+
+let rootElement = document.getElementById('root');
+ReactDOM.render(
+  <BrowserRouter>
+    <Provider store={store}>
+      <NavBar/>
+      <div className="container">
+        <Switch>
+          <App/>
+        </Switch>
+      </div>
+    </Provider>
+  </BrowserRouter>, rootElement
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
